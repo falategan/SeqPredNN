5eb4f188f37030f71622000d1cdc21426276cb7e
falategan <95366074+falategan@users.noreply.github.com>
2021-12-10 15:11:26 +0200

    Create neural_net module
    Moved NeuralNetwork and StructureDataset classes from train_model.py to neural_net.py

--- train_model.py
+++ train_model.py
@@ -3,10 +3,10 @@ import pathlib
 import argparse
 from torch import nn
 from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
 from sklearn.metrics import classification_report, cohen_kappa_score
 from sampling import Sampling
 from plots import Plots
+from neural_net import StructureDataset, NeuralNetwork
 
 BATCH_SIZE = 4096
 
@@ -34,47 +34,6 @@ def get_args():
         raise ValueError('The train ratio must be a number between 0 and 1')
     return pathlib.Path(args.input_dir), args.sampling_mode, args.train_ratio, args.test_set, \
            pathlib.Path(args.out_dir), args.epochs
-
-
-class StructureDataset(Dataset):
-    def __init__(self, features):
-        self.labels = features['residue_labels'].long()
-        self.example_features = [torch.flatten(features[key], start_dim=1) for key in ['displacements', 'rotations',
-                                                                                       'torsional_angles']]
-        self.examples = torch.cat(self.example_features, dim=1)
-
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, idx):
-        return self.examples[idx], self.labels[idx]
-
-
-class NeuralNetwork(nn.Module):
-    def __init__(self, input_nodes):
-        super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(input_nodes, 64),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(64, 20)
-        )
-        print('Model layout:\n', self.linear_relu_stack, '\n')
-
-    def forward(self, x):
-        x = self.flatten(x)
-        output_values = self.linear_relu_stack(x)
-        return output_values
 
 
 def train(dataloader, model, loss_fn, optimizer):
